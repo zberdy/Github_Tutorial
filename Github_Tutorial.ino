811ae2c5363b77ca3e7120efbb4e2b9062b4dfbe
Daniel Thiffeault <daniel.thiffeault.2@ens.etsmtl.ca>
2013-11-08 13:40:47 -0500

    commit fake comment

--- Github_Tutorial.ino
+++ Github_Tutorial.ino
@@ -10,7 +10,7 @@
  If we have an analog sensor such as a trimpot or flex sensor on A0, this code would attempt
  to read it and output it to the terminal at 9600bps.
 */
-
+//fake comment
 void setup()
 {
   Serial.begin(9600);
